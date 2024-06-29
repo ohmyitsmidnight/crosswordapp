1a165a704a01701dd7113841764786ea5bdaf874
Lily Liu <lliu@raa.com.au>
2024-06-30 00:44:52 +0930

    hardcoded server port to 3000

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 
 app.use(cors());
 
